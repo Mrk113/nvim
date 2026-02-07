c4c20b1056d7c0f8702dff9e01bdabf3da9ee8eb
Mark Karpov <mark.karpov@gmx.de>
2026-02-07 17:39:57 +0100

    Fix Autopair + Add C LSP + LSP Keybinds

--- init.lua
+++ init.lua
@@ -9,18 +9,19 @@ vim.opt.undofile = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
-vim.opt.cursorline = true 
+vim.opt.cursorline = true
 
 vim.opt.wrap = false
 vim.opt.colorcolumn = "80"
 
 vim.opt.clipboard = "unnamedplus"
 
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
---Keybinds
 -- Move selected lines up/down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 --Autocommands
 local autocmd = vim.api.nvim_create_autocmd
@@ -57,35 +58,62 @@ vim.pack.add({
     --Windowing
     { src = gh('nvim-lua/plenary.nvim') },
     { src = gh('nvim-telescope/telescope.nvim') },
-    { src = gh('ThePrimeagen/harpoon'), version = "harpoon2" },
+    { src = gh('ThePrimeagen/harpoon'),           version = "harpoon2" },
 })
 
 --Syntax Highligthing
-require("nvim-treesitter").setup({})
+require("nvim-treesitter").setup()
 
 --LSP
-require("mason").setup({})
-require("mason-lspconfig").setup({})
+require("mason").setup()
+require("mason-lspconfig").setup(
+    {
+        ensure_installed = {
+            "lua_ls",
+            "clangd",
+        }
+    }
+)
+
 vim.diagnostic.config({
-  virtual_text = true,
+    virtual_text = true,
 })
+
+map("n", "<leader>fm", function()
+    vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer (LSP)" })
+
+
+-- Navigation
+map("n", "gd", vim.lsp.buf.definition, opts)
+map("n", "gi", vim.lsp.buf.implementation, opts)
+map("n", "gr", vim.lsp.buf.references, opts)
+map("n", "gt", vim.lsp.buf.type_definition, opts)
+
+-- Info
+map("n", "K", vim.lsp.buf.hover, opts)
+map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+-- Refactor
+map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 
 --Auto Complete
 local cmp = require("cmp")
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<C-f>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-  }),
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "path" },
-  },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-f>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    }),
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+    },
 })
-require("nvim-autopairs").setup({})
+require("nvim-autopairs").setup()
 
 --Colortheme
 vim.cmd.colorscheme("catppuccin")
@@ -98,22 +126,22 @@ require("telescope").setup({})
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
+map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+map("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+map("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
 
 local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", function()
-  harpoon:list():add()
+map("n", "<leader>a", function()
+    harpoon:list():add()
 end)
 
-vim.keymap.set("n", "<leader>h", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
+map("n", "<leader>h", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+map("n", "<leader>1", function() harpoon:list():select(1) end)
+map("n", "<leader>2", function() harpoon:list():select(2) end)
+map("n", "<leader>3", function() harpoon:list():select(3) end)
+map("n", "<leader>4", function() harpoon:list():select(4) end)
